4c53e9d24bcd827746f59d8ff1fa623787b1aedb
Christopher Hall <17876556+hallgchris@users.noreply.github.com>
2022-09-24 11:26:46 +1000

    Try to deploy with gh actions

--- vis-website/next.config.js
+++ vis-website/next.config.js
@@ -1,7 +1,12 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: { unoptimized: true },
+  assetPrefix: "/fit3179-dv2/",
+  basePath: "/fit3179-dv2",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
