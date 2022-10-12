e39803d844f9dc1fe42932698b47734e5b7e5579
Christopher Hall <17876556+hallgchris@users.noreply.github.com>
2022-10-12 23:00:53 +1100

    Start fiddling with layout

--- vis-website/pages/index.tsx
+++ vis-website/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import type { NextPage } from "next";
 import CumulativeCapacityPlot from "../components/CumulativeCapacityPlot";
 import HouseholdInstallationMap from "../components/HouseholdInstallationMap";
@@ -8,15 +8,45 @@ import StateSizeChart from "../components/StateSizeChart";
 const Home: NextPage = () => {
   return (
     <Container maxWidth="xl">
-      <h1>Solar in Australia</h1>
-      <h2>Residential solar usage depends on location</h2>
-      <HouseholdInstallationMap />
-      <h2>The size of solar installations has increased over time</h2>
-      <InstallationSizeHeatmap />
-      <h2>The size of solar plants varies between states</h2>
-      <StateSizeChart />
-      <h2>Australia is increasingly a solar-powered country</h2>
-      <CumulativeCapacityPlot />
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <h1>Solar in Australia</h1>
+        </Grid>
+
+        <Grid item xs={12}>
+          <h2>Residential solar usage depends on location</h2>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          This is my first bit of text
+        </Grid>
+        <Grid item xs={12} md={6}>
+          This is my second bit of text
+        </Grid>
+        <Grid item xs={12}>
+          <HouseholdInstallationMap />
+        </Grid>
+
+        <Grid item xs={12}>
+          <h2>The size of solar installations has increased over time</h2>
+        </Grid>
+        <Grid item xs={12}>
+          <InstallationSizeHeatmap />
+        </Grid>
+
+        <Grid item xs={12}>
+          <h2>The size of solar plants varies between states</h2>
+        </Grid>
+        <Grid item xs={12}>
+          <StateSizeChart />
+        </Grid>
+
+        <Grid item xs={12}>
+          <h2>Australia is increasingly a solar-powered country</h2>
+        </Grid>
+        <Grid item xs={12}>
+          <CumulativeCapacityPlot />
+        </Grid>
+      </Grid>
     </Container>
   );
 };
